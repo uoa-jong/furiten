1dc1d4a85e0760d2cfe5ccfa680624d191cc7963
rikuto <rkt.w2024@outlook.jp>
2017-04-22 04:31:28 +0900

    model fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421185801) do
+ActiveRecord::Schema.define(version: 20170421191643) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "e_userid"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170421185801) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "student_is"
+    t.string   "student_id"
     t.string   "user_name"
     t.string   "password"
     t.integer  "role_id"
