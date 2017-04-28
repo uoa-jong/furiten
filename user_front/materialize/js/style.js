9e5960df4b7e3e736f6a7f9b372d48ee127ee13a
mont-belln <yihy1vk2kd3.0120110715@i.softbank.jp>
2017-04-28 13:03:07 +0900

    nwp

--- user_front/materialize/js/style.js
+++ user_front/materialize/js/style.js
@@ -14,3 +14,8 @@ $(document).ready(function() {
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
 })
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
