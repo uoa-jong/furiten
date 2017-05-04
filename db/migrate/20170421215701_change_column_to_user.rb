39a375402d166fcd893b494c29b46f1f81f32932
rikuto <rkt.w2024@outlook.jp>
2017-05-04 18:35:04 +0900

    migration file changed

--- db/migrate/20170421215701_change_column_to_user.rb
+++ db/migrate/20170421215701_change_column_to_user.rb
@@ -1,11 +1,6 @@
 class ChangeColumnToUser < ActiveRecord::Migration[5.0]
   def up
     rename_column :users, :password, :password_digest
-    change_column :users, :total_score, :decimal, null: false, default: 0
-  end
-
-  def down
-  	change_column :users, :total_score, :float, null: false, default: 0
   end
 
 end
